56c13dca621494be67eae762f1325d8bf2a8e8df
薛愛君 <aichung@xueaijunde-MacBook-Air.local>
2024-02-19 08:53:31 +0800

    寫完整增刪修功能

--- src/components/editProduct.vue
+++ src/components/editProduct.vue
@@ -1,109 +1,166 @@
 <template>
-    <div class="modal-cover d-flex justify-content-center align-items-center" >
-        <div class="card" >
-            <div class="border d-flex justify-content-between  align-items-center p-3">
-                <div class="bordor fs-20">編輯商品</div>
-                <button type="button" class="btn close" @click="closeModal">x 
-                </button>
-                
-            </div>
-            <table class="table">
-                <thead>
-                <tr class="text-center border-bottom">
-                    <th scope="col">商品名稱</th>
-                    <th scope="col">商品圖片</th>
-                    <th scope="col">商品類型</th>
-                    <th scope="col">價格</th>
-                    <th scope="col">庫存</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="item in productList" :key="item.title">
-                    <td>
-                    <input type="text" v-model="item.title" />
-                    </td>
-                    <td>
-                    <div class="d-flex justify-content-center">
-                        <img
-                        src="../assets/img/機械暴龍獸.jpg"
-                        alt=""
-                        class="w-75"
-                        />
-                    </div>
-                    </td>
-                    <td>
-                    <input type="text" v-model="item.type" />
-                    </td>
-                    <td>
-                    <input type="text" v-model="item.price" />
-                    </td>
-                    <td>
-                    <input type="text" v-model="item.amount" />
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-            <div class="d-flex justify-content-center align-items-center mt-3 p-3">
-            <button type="button" class="btn ">儲存</button>
-            </div>
+  <div class="modal-cover d-flex justify-content-center align-items-center">
+    <div class="wrap">
+      <div class="border d-flex justify-content-between align-items-center p-3">
+        <div class="bordor fs-20 p-2">編輯商品</div>
+        <button type="button" class="btn close" @click="closeModal">x</button>
+      </div>
+      <form class="form">
+        <div class="form-group row mt-3">
+          <label for="productName" class="col-sm-2 col-form-label"
+            >商品名稱</label
+          >
+          <div class="col-sm-10">
+            <input
+              type="text"
+              class="form-control"
+              id="productName"
+              v-model="productDetail.title"
+            />
+          </div>
         </div>
+        <div class="form-group row mt-3">
+          <label for="productImg" class="col-sm-2 col-form-label"
+            >商品圖片</label
+          >
+          <div class="col-sm-10">
+            <img
+              src="../assets/img/defaultImg.jpg"
+              style="max-width: 200px; height: 100px"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div class="form-group row mt-3">
+          <label for="productType" class="col-sm-2 col-form-label"
+            >商品類型</label
+          >
+          <div class="col-sm-10">
+            <input
+              type="text"
+              class="form-control"
+              id="productType"
+              v-model="productDetail.type"
+            />
+          </div>
+        </div>
+
+        <div class="form-group row mt-3">
+          <label for="productPrice" class="col-sm-2 col-form-label">價格</label>
+          <div class="col-sm-10">
+            <input
+              type="number"
+              class="form-control"
+              id="productPrice"
+              v-model="productDetail.price"
+            />
+          </div>
+        </div>
+
+        <div class="form-group row mt-3">
+          <label for="productStone" class="col-sm-2 col-form-label">庫存</label>
+          <div class="col-sm-10">
+            <input
+              type="number"
+              class="form-control"
+              id="productStone"
+              v-model="productDetail.stock"
+            />
+          </div>
+        </div>
+      </form>
+
+      <div class="d-flex justify-content-center align-items-center mt-3 p-3">
+        <button type="button" @click="submit" class="btn btn-green text-white">
+          儲存
+        </button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    
-props: {
-  productList: {
-    type: Array,
-    required: true
+  props: {
+    productListLength: {
+      type: Number,
+      required: true,
+    },
+    eidtProductDetail: {
+      type: Object,
+    },
   },
-  showAddModal: {
-    type: Boolean,
-    required: true
+  emits: ["closeModal", "addProduct", "cleanEditProduct"],
+  data() {
+    return {
+      productDetail: {
+        title: "",
+        type: "",
+        price: "",
+        stock: "",
+      },
+    };
   },
-  showEditModal: {
-    type: Boolean,
-    required: true
-  }
-  
-},
-methods:{
+  methods: {
     closeModal() {
-            this.$emit('close-modal'); 
-            //  props in  , emit out
-        }
-},
-
-}
-
+      this.$emit("closeModal");
+    },
+    submit() {
+      if (this.eidtProductDetail) {
+        this.$emit("updateProduct", this.productDetail);
+      } else {
+        this.productDetail.id = this.productListLength + 1;
+        this.$emit("addProduct", this.productDetail);
+      }
+      this.closeModal();
+    },
+  },
+  mounted() {
+    // this.productDetail = {
+    //   ...this.eidtProductDetail
+    // }
+    if (this.eidtProductDetail) {
+      Object.assign(this.productDetail, this.eidtProductDetail);
+    }
+  },
+  unmounted() {
+    this.$emit("cleanEditProduct");
+  },
+};
 </script>
 
 <style scoped>
-
 .modal-cover {
-position: fixed;
-top: 0;
-left: 0;
-width: 100%;
-height: 100%;
-background-color: rgba(0, 0, 0, 0.5); 
-z-index: 999; 
-
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
 }
-.card{
-z-index: 888;
-background-color: #fff;
-width: 80%;
-height: 80vh;
-}
-
-.close{
-color: #999EA2;
+.wrap {
+  z-index: 888;
+  background-color: #fff;
+  width: 80%;
+  height: 80vh;
 }
 
+.form {
+  max-height: 380px;
+  overflow-y: auto;
+}
+
+.close {
+  color: #999ea2;
+}
 
 .close:hover {
-color: #000;
+  color: #000;
+}
+
+.btn-green {
+  background-color: #a5df86;
 }
 </style>
--- src/view/Product.vue
+++ src/view/Product.vue
@@ -1,12 +1,54 @@
 <template>
   <div class="mt-5">
-    <table class="table bg-light rounded text-center"> 
+    <table class="table bg-light rounded text-center">
       <thead>
         <tr>
-          <th scope="col" v-for="item in headers" :key="item"> {{item}} </th>   
+          <th scope="col" v-for="item in headers" :key="item">{{ item }}</th>
         </tr>
       </thead>
-      <productItem/>
+      <tbody>
+        <tr v-for="item in productList" :key="item.title">
+          <th scope="row">{{ item.title }}</th>
+          <td>
+            <img class="h-50 w-50" />
+          </td>
+          <td>{{ item.type }}</td>
+          <td>{{ item.price }}</td>
+          <td>{{ item.amount }}</td>
+          <td>
+            <button
+              type="button"
+              class="btn btn-warning text-white"
+              @click="openEditModal(item)"
+            >
+              編輯
+            </button>
+            <button
+              type="button"
+              class="btn btn-danger m-lg-3"
+              @click="deleteProduct(item)"
+            >
+              刪除
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="6" class="text-center">
+            <button class="plus-set rounded-3" @click="openEditModal">
+              <i class="fas fa-plus fa-1x"></i>
+            </button>
+          </td>
+        </tr>
+
+        <editProduct
+          v-if="showEditModal"
+          :productListLength="productList.length"
+          :eidtProductDetail="eidtProductDetail"
+          @closeModal="showEditModal = false"
+          @addProduct="addProductHandle"
+          @cleanEditProduct="eidtProductDetail = null"
+        />
+      </tbody>
     </table>
     <div class="d-flex justify-content-center">
       <pagination />
@@ -16,80 +58,112 @@
 
 <script>
 import pagination from "@/components/pagination.vue";
-import productItem from "@/components/productItem.vue";
-
+import editProduct from "@/components/editProduct.vue";
 
 export default {
   components: {
     pagination,
-    productItem
+    editProduct,
   },
 
   data() {
     return {
-      headers:['商品名稱','商品圖片','商品類型','價格','庫存','操作']
-
-      ,
+      eidtProductDetail: null,
+      headers: ["商品名稱", "商品圖片", "商品類型", "價格", "庫存", "操作"],
+      showEditModal: false,
+      productList: [
+        {
+          id: 1,
+          title: "機械暴龍獸",
+          type: "模型",
+          price: 1950,
+          stock: 10,
+        },
+        {
+          id: 2,
+          title: "胖胖星球獸",
+          type: "模型",
+          price: 10,
+          stock: 10,
+        },
+        {
+          id: 3,
+          title: "貓貓星球獸",
+          type: "模型",
+          price: 10,
+          stock: 10,
+        },
+        {
+          id: 4,
+          title: "咪咪星球獸",
+          type: "模型",
+          price: 10,
+          stock: 10,
+        },
+      ],
     };
   },
 
   methods: {
+    openEditModal(item) {
+      // 傳item給子層
+      this.eidtProductDetail = item;
+      this.showEditModal = true;
+      console.log(item);
+      // 編輯
+    },
+    // 刪除
+    deleteProduct(item) {
+      const index = this.productList.indexOf(item);
+      console.log(index);
+      if (index !== -1) {
+        this.productList.splice(index, 1);
+      }
+    },
+    addProductHandle(newProduct) {
+      this.productList.push(newProduct);
+    },
+    //編輯
+    updateProduct(updatedProduct) {
+      //
+      const index = this.productList.findIndex(
+        (item) => item.id === updatedProduct.id
+      );
 
+      if (index !== -1) {
+        this.productList.splice(index, 1, updatedProduct);
+        console.log(this.productList);
+      }
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-  button i {
-    border: 0px #fff solid;
-  }
+button i {
+  border: 0px #fff solid;
+}
+.modal-cover {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+}
+.card {
+  z-index: 888;
+  background-color: #fff;
+  width: 80%;
+  height: 80vh;
+}
+
+.close {
+  color: #999ea2;
+}
+
+.close:hover {
+  color: #000;
+}
 </style>
-  <!-- <div class="modal-cover d-flex justify-content-center align-items-center" v-if="showModal">
-            <div class="card">
-                <div class="border d-flex justify-content-between  align-items-center p-3">
-                    <div class="bordor fs-20">編輯商品</div>
-                    <button type="button" class="btn close" aria-label="Close">x 
-                    </button>
-                    
-                </div>
-                <table class="table">
-                    <thead>
-                    <tr class="text-center border-bottom">
-                        <th scope="col">商品名稱</th>
-                        <th scope="col">商品圖片</th>
-                        <th scope="col">商品類型</th>
-                        <th scope="col">價格</th>
-                        <th scope="col">庫存</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-for="item in productList" :key="item" class="">
-                        <td>
-                        <input type="text" v-model="item.title" />
-                        </td>
-                        <td>
-                        <div class="d-flex justify-content-center">
-                            <img
-                            src="../assets/img/機械暴龍獸.jpg"
-                            alt=""
-                            class="w-75"
-                            />
-                        </div>
-                        </td>
-                        <td>
-                        <input type="text" v-model="item.type" />
-                        </td>
-                        <td>
-                        <input type="text" v-model="item.price" />
-                        </td>
-                        <td>
-                        <input type="text" v-model="item.amount" />
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-                <div class="d-flex justify-content-center align-items-center border mt-3 p-3">
-                <button type="button" class="btn ">儲存</button>
-                </div>
-            </div>
-        </div> -->
